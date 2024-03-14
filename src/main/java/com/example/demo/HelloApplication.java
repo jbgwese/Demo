696120bb265edc3e742ddb7d66ae9db28bfe65a6
Job Gwese <job.gwese@higherlifefoundation.org>
2024-03-14 15:18:25 +0200

    second

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("First try!");
+        stage.setTitle("Second try!");
         stage.setScene(scene);
         stage.show();
     }
